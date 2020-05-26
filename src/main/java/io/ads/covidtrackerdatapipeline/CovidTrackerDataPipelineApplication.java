94601cd54f4e0160fd1460198fa73b12df6d90bc
Amandeep Singh <amandeepsingh@Amandeeps-MacBook-Pro.local>
2020-05-25 21:22:53 -0700

    Initial commit

---
+++ src/main/java/io/ads/covidtrackerdatapipeline/CovidTrackerDataPipelineApplication.java
@@ -0,0 +1,13 @@
+package io.ads.covidtrackerdatapipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTrackerDataPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidTrackerDataPipelineApplication.class, args);
+	}
+
+}
